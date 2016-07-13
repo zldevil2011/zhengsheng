7e62036f0f891472d64e9203e8668fa21ccbe3b4
zldevil2011 <929034478@qq.com>
2016-07-13 16:51:06 +0800

    fix bug

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 #coding=utf-8
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from app.views import index
+from app.views import index, user
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="app/welcome.html")),
     url(r'^admin_login/$', index.admin_login, name="admin_login"),
     url(r'^admin_index/$', index.admin_index, name="admin_index"),
+    url(r'^admin_account/$', user.admin_account, name="admin_account"),
 ]
---
+++ app/views/user.py
@@ -0,0 +1,11 @@
+
+# coding=utf-8
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+
+
+@csrf_exempt
+def admin_account(request):
+    return render(request, 'app/admin_account.html', {})
