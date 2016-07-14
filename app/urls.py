b5f9d514150b459c9cc715d3c5e23953f943dc85
zldevil2011 <929034478@qq.com>
2016-07-14 14:28:56 +0800

    childmenu

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 #coding=utf-8
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from app.views import index, user
+from app.views import index, user, device
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="app/welcome.html")),
     url(r'^admin_login/$', index.admin_login, name="admin_login"),
     url(r'^admin_index/$', index.admin_index, name="admin_index"),
     url(r'^admin_account/$', user.admin_account, name="admin_account"),
     url(r'^admin_work_order/$', user.admin_work_order, name="admin_work_order"),
+    url(r'^admin_device/$', device.admin_device, name="admin_device"),
 ]
