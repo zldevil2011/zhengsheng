6bc95fcdfd64e4673a10f8cedb332f58a1291a3b
zldevil2011 <929034478@qq.com>
2016-07-23 11:28:08 +0800

    add charts

--- app/terminal_urls.py
+++ app/terminal_urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 from app.terminalView import index
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="terminalUser/base.html")),
+    url(r'electricity_info/$', TemplateView.as_view(template_name="terminalUser/terminal_electricity_info.html")),
     url(r'index/$', index.index, name="terminal-index"),
 ]
