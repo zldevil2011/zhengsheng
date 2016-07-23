84e28e783096738c441a5877554edaefd06578df
zldevil2011 <929034478@qq.com>
2016-07-23 10:53:35 +0800

    terminal index

--- app/terminal_urls.py
+++ app/terminal_urls.py
@@ -1,7 +1,8 @@
 #coding=utf-8
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from app.views import index, user, device, data, mobile, installArea, admin_info
+from app.terminalView import index
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="terminalUser/base.html")),
+    url(r'index/$', index.index, name="terminal-index"),
 ]
