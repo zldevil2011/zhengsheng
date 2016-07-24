8c7aba213be069489bae38e63749d6ac626d8cd2
zldevil2011 <929034478@qq.com>
2016-07-24 14:09:06 +0800

    terminal_fees template

--- app/terminal_urls.py
+++ app/terminal_urls.py
@@ -5,5 +5,6 @@ from app.terminalView import index
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="terminalUser/base.html")),
     url(r'electricity_info/$', TemplateView.as_view(template_name="terminalUser/terminal_electricity_info.html")),
+    url(r'fees/$', TemplateView.as_view(template_name="terminalUser/terminal_fees.html")),
     url(r'index/$', index.index, name="terminal-index"),
 ]
