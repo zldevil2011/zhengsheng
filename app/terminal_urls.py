7c07dd00fe7f39b66f0df76a7f90bc6e70b0fae5
zldevil2011 <929034478@qq.com>
2016-07-24 14:23:11 +0800

    details

--- app/terminal_urls.py
+++ app/terminal_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="terminalUser/base.html")),
     url(r'electricity_info/$', TemplateView.as_view(template_name="terminalUser/terminal_electricity_info.html")),
     url(r'fees/$', TemplateView.as_view(template_name="terminalUser/terminal_fees.html")),
+    url(r'details/$', TemplateView.as_view(template_name="terminalUser/terminal_details.html")),
     url(r'index/$', index.index, name="terminal-index"),
 ]
