29ed734da334629bbaa684ce86ee01e8428eb66b
zldevil2011 <929034478@qq.com>
2016-07-24 21:56:37 +0800

    terminal login template

--- app/terminal_urls.py
+++ app/terminal_urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 from app.terminalView import index, month_fees, user_work_order
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="terminalUser/base.html")),
+    url(r'login/$', TemplateView.as_view(template_name="terminalUser/terminal_login.html")),
     url(r'electricity_info/$', TemplateView.as_view(template_name="terminalUser/terminal_electricity_info.html")),
     url(r'fees/$', TemplateView.as_view(template_name="terminalUser/terminal_fees.html")),
     url(r'details/$', TemplateView.as_view(template_name="terminalUser/terminal_details.html")),
