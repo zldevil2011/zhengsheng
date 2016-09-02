f04043a38aef0a3e03de7d875470ac92a2847871
zldevil2011 <929034478@qq.com>
2016-09-02 16:54:27 +0800

    add detail data

--- app/terminalView/device.py
+++ app/terminalView/device.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-from app.models import AppUser, Device
+from app.models import AppUser, Device, Data
 from django.contrib.auth.hashers import check_password
 
 
@@ -13,7 +13,9 @@ def terminal_device_info(request):
     except:
         return HttpResponseRedirect("/terminal/user/login/")
     device = Device.objects.get(appuser=user)
+    data = Data.objects.get(device=device)
     return render(request, 'terminalUser/terminal_device_info.html', {
         'user': user,
         'device': device,
+        'data': data,
     })
