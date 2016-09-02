347afc81199fd96f87edfb5b2a42e723f5bb3928
zldevil2011 <929034478@qq.com>
2016-09-02 16:07:37 +0800

    add ele info

--- app/terminalView/power.py
+++ app/terminalView/power.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-from app.models import AppUser
+from app.models import AppUser, Device, Data
 from django.contrib.auth.hashers import check_password
 
 
@@ -12,6 +12,11 @@ def electricity_info(request):
         user = AppUser.objects.get(username=request.session['username'])
     except:
         return HttpResponseRedirect("/terminal/user/login/")
+    device = Device.objects.get(appuser=user)
+    data = Data.objects.filter(device=device)[0]
+
     return render(request, 'terminalUser/terminal_electricity_info.html', {
         'user': user,
+        'device': device,
+        'data': data,
     })
