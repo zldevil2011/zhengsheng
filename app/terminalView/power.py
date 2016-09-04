183cb673872dbeb7bb9664a15eb3c52420829479
zldevil2011 <929034478@qq.com>
2016-09-04 22:14:29 +0800

    add month & day data

--- app/terminalView/power.py
+++ app/terminalView/power.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import date
 import calendar
-
+import json
 
 @csrf_exempt
 def electricity_info(request):
@@ -40,34 +40,46 @@ def electricity_info(request):
             break
         if data.time == init_time:
             today_power.append(data.power)
-            today_hour.append(data.time.hour)
+            today_hour.append(str(data.time.hour) + "时")
             init_time = init_time - hour_delta
+    today_hour.reverse()
+    today_power.reverse()
     print today_power
-
+    today_data = {}
+    today_data["today_power"] = today_power
+    today_data["today_hour"] = today_hour
     # 计算当月数据
-    day += 1
+    day += 12
     latest_data = datas[0]
-    month_days = calendar.monthrange(year, day)[1]
+    # month_days = calendar.monthrange(year, day)[1]
     month_power = []
     month_day = []
-    month_day.append(latest_data.time.day)
+    month_day.append(str(latest_data.time.day) + "号")
     month_power.append(latest_data.power)
     init_time = datetime(year, month, day, 23, 50, 0) - timedelta(days=1)
     end_time = datetime(year, month, 1, 23, 50, 0)
+    print init_time
+    print end_time
     day_delta = timedelta(days=1)
     for data in datas:
         if data.time == end_time:
             month_power.append(data.power)
-            month_day.append(data.time.day)
+            month_day.append(str(data.time.day) + "号")
             break
         if data.time == init_time:
             month_power.append(data.power)
-            month_day.append(data.time.day)
+            month_day.append(str(data.time.day) + "号")
             init_time = init_time - day_delta
     print month_power
-
+    month_power.reverse()
+    month_day.reverse()
+    month_data = {}
+    month_data["month_power"] = month_power
+    month_data["month_day"] = month_day
     return render(request, 'terminalUser/terminal_electricity_info.html', {
         'user': user,
         'device': device,
         'data': data,
+        'today_data': json.dumps(today_data, "application/json"),
+        'month_data': json.dumps(month_data, "application/json")
     })
