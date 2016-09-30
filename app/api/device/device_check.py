d493a0f2f2282ee4743f9c3e845037b933ac7015
xiaolong_xl <929034478@qq.com>
2016-09-30 09:42:58 +0800

    key -> k

--- app/api/device/device_check.py
+++ app/api/device/device_check.py
@@ -9,16 +9,23 @@ import time
 class DeviceCheck(APIView):
 
     def post(self, request, format=None):
-        device_id = int(request.data.get('id', 0))
+        request_str = request.data['k']
+        request_val_list = request_str.split('=')
+        print request_val_list
+        device_id = int(request_val_list[1])
         print "device_id = ", device_id
         # comment_content = request.data['comment_content']
         timeF = '%Y-%m-%d %X'
         CurTime = time.strftime(timeF, time.localtime())
         try:
             device = Device.objects.get(device_id = device_id)
-            ext_info = 0
+            ext_info = '0'
             if device.city_code is None or device.village_code is None or device.gateway_code is None:
-                ext_info = 1
-            return Response({'valid_s': 1, 'CurTime': CurTime, 'ext_info': ext_info},status=status.HTTP_200_OK)
+                ext_info = '1'
+            response_str = ''
+            response_str += 'valid_s=1;CurTime='+CurTime+';ext_info='+ext_info
+            return Response({'k': response_str} , status=status.HTTP_200_OK)
         except:
-            return Response({'valid_s': 0, 'CurTime':CurTime, 'ext_info': '1'}, status=status.HTTP_404_NOT_FOUND)
+            response_str = ''
+            response_str += 'valid_s=0;CurTime=' + CurTime + ';ext_info=1'
+            return Response({'k': response_str}, status=status.HTTP_404_NOT_FOUND)
