1d8dd3571201ac372252d93017864783631b7256
xiaolong_xl <929034478@qq.com>
2016-10-06 17:29:26 +0800

    repaint for my device

--- app/terminalView/device.py
+++ app/terminalView/device.py
@@ -13,41 +13,27 @@ def terminal_device_info(request):
         return HttpResponseRedirect("/terminal/user/login/")
     device = user.device
     try:
-        data = Data.objects.filter(device_id=device).order_by('-time')[0]
+        data = Data.objects.filter(device_id=device).order_by('-powerT')[0]
     except:
         data = None
     # 获取当前设备的今日温度曲线
     now = datetime.now()
-    today_now = datetime(now.year, now.month, now.day, now.hour, now.minute, 0)
-    today_start = datetime(now.year, now.month, now.day, 0, 0, 0)
-    datas = Data.objects.filter(powerT__lte=today_now, powerT__gte=today_start).order_by('-powerT')
+    today = date.today()
+    year = today.year
+    month = today.month
+    day = today.day
+    datas = Data.objects.filter(tempT__year=year, tempT__month=month, tempT__day=day).order_by('-tempT')
+
     temperature = []
     for d in datas:
-        temperature.append(d.temperature)
+        temperature.append(d.temp)
     print temperature
 
-
-    time_delta_hour = timedelta(hours = 1)
-    # 获取当前设备的电压曲线，按小时计
-    start_time_hour0 = datetime(now.year, now.month, now.day, 0, 0, 0)
-    voltage = []
-    for d in datas:
-        if d.time == start_time_hour0:
-            voltage.append(d.voltage)
-            start_time_hour0 += time_delta_hour
-
-    # 获取当前设备的电流曲线，按小时计
-    start_time_hour0 = datetime(now.year, now.month, now.day, 0, 0, 0)
-    electricity = []
-    for d in datas:
-        if d.time == start_time_hour0:
-            electricity.append(d.electricity)
-            start_time_hour0 += time_delta_hour
     return render(request, 'terminalUser/terminal_device_info.html', {
         'user': user,
         'device': device,
         'data': data,
         'temperature': temperature,
-        'voltage': voltage,
-        'electricity': electricity,
+        'voltage': temperature,
+        'electricity': temperature,
     })
