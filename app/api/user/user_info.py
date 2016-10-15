fef0df0826407253f4c551b71cdea8da10ab9c1b
xiaolong_xl <929034478@qq.com>
2016-10-15 11:06:06 +0800

    post to get user_info

--- app/api/user/user_info.py
+++ app/api/user/user_info.py
@@ -8,6 +8,16 @@ from app.serializer import AppUserSerializer
 
 class UserInfo(APIView):
     def get(self, request, format=None):
-        user = AppUser.objects.get(pk=1)
+        user = AppUser.objects.get(pk=3)
         serializer = AppUserSerializer(user)
         return Response({'user':serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        print request.data
+        try:
+            user_id = int(request.data['user_id'])
+            user = AppUser.objects.get(pk=user_id)
+            serializer = AppUserSerializer(user)
+            return Response({'user': serializer.data}, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_403_FORBIDDEN)
