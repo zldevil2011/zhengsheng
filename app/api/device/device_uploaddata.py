88fe58654e784f2844b9df3dc86d60b55676df33
xiaolong_xl <929034478@qq.com>
2016-11-25 12:08:18 +0800

    fix bug

--- app/api/device/device_uploaddata.py
+++ app/api/device/device_uploaddata.py
@@ -39,20 +39,20 @@ class DeviceUploadData(APIView):
                             print key, ":", val
                             now = datetime.today()
                             now = datetime(now.year, now.month, now.day, now.hour, now.minute, now.second)
-                            if key == "temp":
-                                new_data.temp = float(val)
+                            if key == "temp":  # 温度
+                                new_data.temp = float(val)/10.0
                                 new_data.tempT = now
-                            elif key == "tempT":
+                            elif key == "tempT": # 温度采集时间
                                 now = datetime.now()
                                 new_data.tempT = now
                                 # new_data.tempT = datetime.strptime(str(val), "%Y-%m-%d %H:%M:%S")
-                            elif key == "powerV":
-                                new_data.powerV = float(val)/1000
+                            elif key == "powerV": # 有功电能
+                                new_data.powerV = float(val)/1000.0
                                 new_data.powerT = now
-                            elif key == "powerI":
-                                new_data.powerI = float(val)/1000
+                            elif key == "powerI": # 无功电能
+                                new_data.powerI = float(val)/1000.0
                                 new_data.powerT = now
-                            elif key == "powerT":
+                            elif key == "powerT": # 电能采集时间
                                 now = datetime.now()
                                 new_data.powerT = now
                                 # new_data.powerT = datetime.strptime(str(val), "%Y-%m-%d %H:%M:%S")
@@ -70,15 +70,15 @@ class DeviceUploadData(APIView):
                                 now = datetime.now()
                                 new_data.faultBT = now
                                 new_data.faultBT = datetime.strptime(str(val), "%Y-%m-%d %H:%M:%S")
-                            elif key == "vol":
+                            elif key == "vol": # 终端电压
                                 new_data.voltage = float(val)/10.0
-                            elif key == "elec":
+                            elif key == "elec": # 终端电流
                                 new_data.electric_current = float(val)/10.0
-                            elif key == "powerF":
+                            elif key == "powerF": # 功率因数
                                 new_data.power_factor = float(val)/1000.0
-                            elif key == "powerA":
+                            elif key == "powerA": # 有功功率
                                 new_data.active_power = float(val)
-                            elif key == "powerR":
+                            elif key == "powerR": # 无功功率
                                 new_data.reactive_power = float(val)
                             elif key == "time":
                                 now = datetime.now()
@@ -95,51 +95,51 @@ class DeviceUploadData(APIView):
                             val = data.split('=')[1].strip()
                             print key, ":", val
                             if key == "a_vol":
-                                relay.a_voltage = float(val)/10.0
+                                relay.a_voltage = float(val)/10.0  # A相电压
                             elif key == "b_vol":
-                                relay.b_voltage = float(val)/10.0
+                                relay.b_voltage = float(val)/10.0  # B相电压
                             elif key == "c_vol":
-                                relay.c_voltage = float(val)/10.0
+                                relay.c_voltage = float(val)/10.0  # C相电压
                             elif key == "a_elec":
-                                relay.a_electric_current = float(val)/10.0
+                                relay.a_electric_current = float(val)/10.0  # A相电流
                             elif key == "b_elec":
-                                relay.b_electric_current = float(val)/10.0
+                                relay.b_electric_current = float(val)/10.0  # B相电流
                             elif key == "c_elec":
-                                relay.c_electric_current = float(val)/10.0
+                                relay.c_electric_current = float(val)/10.0  # C相电流
                             elif key == "a_powerF":
-                                relay.a_power_factor = float(val)/1000.0
+                                relay.a_power_factor = float(val)/1000.0   # A相功率因数
                             elif key == "b_powerF":
-                                relay.b_power_factor = float(val)/1000.0
+                                relay.b_power_factor = float(val)/1000.0  # B相功率因数
                             elif key == "c_powerF":
-                                relay.c_power_factor = float(val)/1000.0
+                                relay.c_power_factor = float(val)/1000.0  # C相功率因数
                             elif key == "a_powerA":
-                                relay.a_active_power = float(val)
+                                relay.a_active_power = float(val)    # A相有功功率
                             elif key == "b_powerA":
-                                relay.b_active_power = float(val)
+                                relay.b_active_power = float(val)    # B相有功功率
                             elif key == "c_powerA":
-                                relay.c_active_power = float(val)
+                                relay.c_active_power = float(val)    # C相有功功率
                             elif key == "a_powerR":
-                                relay.a_reactive_power = float(val)
+                                relay.a_reactive_power = float(val)   # A相无功功率
                             elif key == "b_powerR":
-                                relay.b_reactive_power = float(val)
+                                relay.b_reactive_power = float(val)   # B相无功功率
                             elif key == "c_powerR":
-                                relay.c_reactive_power = float(val)
+                                relay.c_reactive_power = float(val)   # C相无功功率
                             elif key == "a_powerV":
-                                relay.a_powerV = float(val)/1000.0
+                                relay.a_powerV = float(val)/1000.0    # A相有功电能
                             elif key == "b_powerV":
-                                relay.b_powerV = float(val)/1000.0
+                                relay.b_powerV = float(val)/1000.0    # B相有功电能
                             elif key == "c_powerV":
-                                relay.c_powerV = float(val)/1000.0
+                                relay.c_powerV = float(val)/1000.0    # C相有功电能
                             elif key == "a_powerI":
-                                relay.a_powerI = float(val)/1000.0
+                                relay.a_powerI = float(val)/1000.0    # A相无功电能
                             elif key == "b_powerI":
-                                relay.b_powerI = float(val)/1000.0
+                                relay.b_powerI = float(val)/1000.0    # B相无功电能
                             elif key == "c_powerI":
-                                relay.c_powerI = float(val)/1000.0
+                                relay.c_powerI = float(val)/1000.0    # C相无功电能
                             elif key == "t_powerV":
-                                relay.t_powerV = float(val)/1000.0
+                                relay.t_powerV = float(val)/1000.0    # 总有功电能
                             elif key == "t_powerI":
-                                relay.t_powerI = float(val)/1000.0
+                                relay.t_powerI = float(val)/1000.0    # 总无功电能
                             elif key == "time":
                                 now = datetime.now()
                                 relay.data_time = now
