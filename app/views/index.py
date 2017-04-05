7bca3adc02cf0d120e096357e06d24b009882fa0
xiaolong_xl <929034478@qq.com>
2017-04-05 22:19:19 +0800

    add dialog for event(temperature)

--- app/views/index.py
+++ app/views/index.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from app.models import AppUser, Adminer, Event
 from django.contrib.auth.hashers import check_password
+from dss.Serializer import serializer
+import time
 
 
 @csrf_exempt
@@ -13,6 +15,9 @@ def admin_index(request):
     except:
         return HttpResponseRedirect("/admin_login/")
     temperature_warning_list = Event.objects.filter(name_no=13).order_by('-time')
+    temperature_warning_list = serializer(temperature_warning_list)
+    for t in temperature_warning_list:
+        t["time"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(t["time"])))
     return render(request, 'app/admin_index.html', {
         "user" : user,
         "temperature_warning_list":temperature_warning_list,
