a76318291462369cd8f332b4ee249be9c4e7f9c6
xiaolong_xl <929034478@qq.com>
2017-04-19 21:21:54 +0800

    set localstorage

--- media/welcomescreen/init.js
+++ media/welcomescreen/init.js
@@ -8,16 +8,38 @@ myapp.init = (function () {
   
   var exports = {};
   
-  document.addEventListener("DOMContentLoaded", function(event) { 
-    // Initialize app
-    var fw7App = new Framework7(),
-      fw7ViewOptions = {
-        dynamicNavbar: true,
-        domCache: true
-      },
-      mainView = fw7App.addView('.view-main', fw7ViewOptions),
-      $$ = Dom7,
-      ipc = new myapp.pages.IndexPageController(fw7App, $$);
+  document.addEventListener("DOMContentLoaded", function(event) {
+      // 判断是否是一次打开
+      try{
+          var storage = window.localStorage;
+          var first = storage.getItem("zhengsheng_welcome");
+          console.log(first);
+          if(first == 1){
+              // 不是第一次打开
+          }else{
+              var fw7App = new Framework7(),
+              fw7ViewOptions = {
+                  dynamicNavbar: true,
+                  domCache: true
+              },
+              mainView = fw7App.addView('.view-main', fw7ViewOptions),
+              $$ = Dom7,
+              ipc = new myapp.pages.IndexPageController(fw7App, $$);
+              storage.setItem("zhengsheng_welcome", 1);
+          }
+      }
+      catch (e){
+          var fw7App = new Framework7(),
+              fw7ViewOptions = {
+                  dynamicNavbar: true,
+                  domCache: true
+              },
+              mainView = fw7App.addView('.view-main', fw7ViewOptions),
+              $$ = Dom7,
+              ipc = new myapp.pages.IndexPageController(fw7App, $$);
+      }
+      // Initialize app
+
   });
   
   return exports;
